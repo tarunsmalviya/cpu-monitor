713c7de6b97537f10a80df7e9bb1e6b5e5a8175d
Tarun Malviya <tarunsmalviya@gmail.com>
2018-07-11 19:11:19 +0530

    HTTP functions added.

--- cpu-monitor.py
+++ cpu-monitor.py
@@ -1,6 +1,19 @@
 import time
 import psutil
 import socket
+import requests
+
+def http_get(endpoint, db, query):
+        host = 'http://localhost:8086/'
+        r = requests.get(url = host + endpoint, params = {'db': db, 'q': query})
+        return r.json()
+
+def http_post(endpoint, db, query):
+        host = 'http://localhost:8086/'
+        r = requests.post(url = host + endpoint, params = {'db': db, 'q': query})
+        return r.json()
+
+# print(http_get('query', 'mydb', 'select * from cpu'))
 
 def get_no_of_open_ports():
         ports = set()
@@ -20,9 +33,14 @@ while True:
         no_of_process = len(psutil.pids())
         no_of_open_ports = get_no_of_open_ports()
 
-        print(name)
-        print(cpu_usage)
-        print(no_of_process)
-        print(no_of_open_ports)
-
+        print('Name             : ' + name)
+        print('CPU Usage        : ' + str(cpu_usage))
+        print('No of Processes  : ' + str(no_of_process))
+        print('No of Open Ports : ' + str(no_of_open_ports))
+        print('\n')
+        
         time.sleep(1)
+
+
+
+
