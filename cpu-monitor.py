5f10249251245f4ce4cd15a21f74c332098c2561
Tarun Malviya <tarunm.cstaff@mech.iitd.ac.in>
2018-07-15 16:33:17 +0530

    CPU stats uploaded to InfluxDB via HTTP API.

--- cpu-monitor.py
+++ cpu-monitor.py
@@ -3,18 +3,29 @@ import psutil
 import socket
 import requests
 
+
+# Function to make HTTP GET request
 def http_get(endpoint, db, query):
         host = 'http://localhost:8086/'
         r = requests.get(url = host + endpoint, params = {'db': db, 'q': query})
-        return r.json()
+        return r
 
-def http_post(endpoint, db, query):
+
+# Function to make HTTP POST request
+def http_post(endpoint, db, data):
+        print('METHOD   : POST')
+
         host = 'http://localhost:8086/'
-        r = requests.post(url = host + endpoint, params = {'db': db, 'q': query})
-        return r.json()
+        print('URL      : ' + host + endpoint + 'db=' + db + '&precision=ms')
+        print('DATA     : ' + data)
 
-# print(http_get('query', 'mydb', 'select * from cpu'))
+        r = requests.post(url = host + endpoint + 'db=' + db, data = data)
+        print('RESPONSE : ' + str(r) + '\n')
 
+        return r
+
+
+# Function to get no of open ports
 def get_no_of_open_ports():
         ports = set()
         connections =  psutil.net_connections(kind='all')
@@ -24,23 +35,32 @@ def get_no_of_open_ports():
         return len(ports)
 
 
-count = psutil.cpu_count(logical=True)
+# Function to get current time in millis
+current_milli_time = lambda: int(round(time.time() * 1000))
 
 
+
+########################################################################
+# Code starts here
+########################################################################
+
+# Create DB in InfluxDB
+print(http_post('query?', '', 'CREATE DATABASE cpu_monitor'))
+
+# Name of host machine
+name = socket.gethostname()
+print('Name: ' + name)
+
+# Continuously sending cpu stats through HTTP POST method to InfluxDB (every 1 second)
 while True:
-        name = socket.gethostname()
-        cpu_usage = psutil.cpu_percent(interval=1, percpu=True)
+        cpu_usage = psutil.cpu_percent(interval=1, percpu=False)
         no_of_process = len(psutil.pids())
         no_of_open_ports = get_no_of_open_ports()
 
-        print('Name             : ' + name)
-        print('CPU Usage        : ' + str(cpu_usage))
-        print('No of Processes  : ' + str(no_of_process))
-        print('No of Open Ports : ' + str(no_of_open_ports))
-        print('\n')
-        
+        query = 'logs,' + \
+                'machine_name=' + name + \
+                ' usage=' + str(cpu_usage) + ',no_of_process=' + str(no_of_process) + ',no_of_open_ports=' + str(no_of_open_ports) + \
+                ' ' + str(current_milli_time())
+        http_post('write?', 'cpu_monitor', query)
+
         time.sleep(1)
-
-
-
-
